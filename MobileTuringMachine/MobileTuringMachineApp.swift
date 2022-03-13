1c676aa5c74ed39c9219a641a05cd899f70ac94b
Snow Lukin <everyoursden@gmail.com>
2022-03-13 16:26:43 +0300

    Initial Commit

---
+++ MobileTuringMachine/MobileTuringMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileTuringMachineApp.swift
+//  MobileTuringMachine
+//
+//  Created by Snow Lukin on 13.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MobileTuringMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
