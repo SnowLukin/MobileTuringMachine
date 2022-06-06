15483e8970f561bb6b4de0d46e126a29b37f6f8e
Snow Lukin <everyoursden@gmail.com>
2022-06-06 12:06:56 +0300

    Cosmetic fixes

--- MobileTuringMachine/View/States/CombinationSettings.swift
+++ MobileTuringMachine/View/States/CombinationSettings.swift
@@ -19,51 +19,8 @@ struct CombinationSettings: View {
     
     var body: some View {
         Form {
-            Section(header: Text("Change to following character")) {
-                Picker(selection: $charIndex) {
-                    ForEach(0..<viewModel.tapes[elementID].alphabetArray.count, id: \.self) { alphabetElementIndex in
-                        
-                        Text(viewModel.tapes[elementID].alphabetArray[alphabetElementIndex])
-                            .tag(alphabetElementIndex)
-                    }
-                } label: {
-                    Text("Alphabet")
-                }
-                .onAppear {
-                    let currentChar = viewModel.states[stateID].options[optionID].combinationsTuple[elementID].toCharacter
-                    let currentCharIndexInTape = Int(viewModel.tapes[elementID].alphabetArray.firstIndex(of: currentChar) ?? 0)
-                    charIndex = currentCharIndexInTape
-                }
-                .onChange(of: charIndex) { newValue in
-                    let newChar = viewModel.tapes[elementID].alphabetArray[newValue]
-                    viewModel.states[stateID].options[optionID].combinationsTuple[elementID].toCharacter = newChar
-                }
-            }
-            
-            Section(header: Text("Move tape's head to following direction")) {
-                Picker(selection: $directionIndex) {
-                    Image(systemName: "arrow.counterclockwise").tag(0)
-                    Image(systemName: "arrow.left").tag(1)
-                    Image(systemName: "arrow.right").tag(2)
-                } label: {
-                    Text("Direction")
-                }
-                .onAppear {
-                    let currentDirection = viewModel.states[stateID].options[optionID].combinationsTuple[elementID].direction
-                    switch currentDirection {
-                    case .stay:
-                        directionIndex = 0
-                    case .left:
-                        directionIndex = 1
-                    case .right:
-                        directionIndex = 2
-                    }
-                }
-                .onChange(of: directionIndex) { newValue in
-                    let newDirection: Direction = newValue == 0 ? .stay : newValue == 1 ? .left : .right
-                    viewModel.states[stateID].options[optionID].combinationsTuple[elementID].direction = newDirection
-                }
-            }
+            characterSection
+            directionSection
         }
         .navigationBarTitle("Tape \(elementID) | Character: \(viewModel.states[stateID].options[optionID].combinationsTuple.map { $0.character }[elementID])")
         .navigationBarTitleDisplayMode(.inline)
@@ -74,5 +31,58 @@ struct CombinationSettings_Previews: PreviewProvider {
     static var previews: some View {
         CombinationSettings(stateID: 0, optionID: 0, elementID: 0)
             .environmentObject(TapeContentViewModel())
+    }
+}
+
+extension CombinationSettings {
+    
+    private var characterSection: some View {
+        Section(header: Text("Change to following character")) {
+            Picker(selection: $charIndex) {
+                ForEach(0..<viewModel.tapes[elementID].alphabetArray.count, id: \.self) { alphabetElementIndex in
+                    
+                    Text(viewModel.tapes[elementID].alphabetArray[alphabetElementIndex])
+                        .tag(alphabetElementIndex)
+                }
+            } label: {
+                Text("Alphabet")
+            }
+            .onAppear {
+                let currentChar = viewModel.states[stateID].options[optionID].combinationsTuple[elementID].toCharacter
+                let currentCharIndexInTape = Int(viewModel.tapes[elementID].alphabetArray.firstIndex(of: currentChar) ?? 0)
+                charIndex = currentCharIndexInTape
+            }
+            .onChange(of: charIndex) { newValue in
+                let newChar = viewModel.tapes[elementID].alphabetArray[newValue]
+                viewModel.states[stateID].options[optionID].combinationsTuple[elementID].toCharacter = newChar
+            }
+        }
+    }
+    
+    private var directionSection: some View {
+        Section(header: Text("Move tape's head to following direction")) {
+            Picker(selection: $directionIndex) {
+                Image(systemName: "arrow.counterclockwise").tag(0)
+                Image(systemName: "arrow.left").tag(1)
+                Image(systemName: "arrow.right").tag(2)
+            } label: {
+                Text("Direction")
+            }
+            .onAppear {
+                let currentDirection = viewModel.states[stateID].options[optionID].combinationsTuple[elementID].direction
+                switch currentDirection {
+                case .stay:
+                    directionIndex = 0
+                case .left:
+                    directionIndex = 1
+                case .right:
+                    directionIndex = 2
+                }
+            }
+            .onChange(of: directionIndex) { newValue in
+                let newDirection: Direction = newValue == 0 ? .stay : newValue == 1 ? .left : .right
+                viewModel.states[stateID].options[optionID].combinationsTuple[elementID].direction = newDirection
+            }
+        }
     }
 }
